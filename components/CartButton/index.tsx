4e1a8e29ba6ad31f6c39cf151469ed44672434e4
Rodolfo Mayora Pereda <rodolfomayora96@gmail.com>
2021-06-24 08:24:56 -0400

    SPA-T17: CartButton Component

--- components/CartButton/index.tsx
+++ components/CartButton/index.tsx
@@ -1,24 +1,33 @@
 import React, { FC } from 'react';
+import { useRouter } from 'next/router';
 import { useCart } from '../../context';
+import { StyledCartButton, Badge, CartIcon } from './styles';
 
 const CartButton: FC = () => {
-
   const cartContext = useCart();
   const { cartState } = cartContext;
   const { cartItemsById } = cartState;
 
   const getCartItemsQuantity = (items: object): number => {
-    
     const allQuantities: Array<number> = Object.values(items);
     const sumQuantities = (acc: number, crr: number): number => acc + crr;
     const total: number = allQuantities.reduce(sumQuantities, 0);
     return total;
   }
 
+  const router = useRouter();
+  const redirect = router.push;
+  const onHandleClick = () => redirect('/Cart');
+
   return (
-    <span>
-      ShoppingCartCount: {getCartItemsQuantity(cartItemsById)}
-    </span>
+    <StyledCartButton onClick={onHandleClick}>
+      <CartIcon src="/images/svg/shoppingCart.svg"
+        alt="Cart"
+        width="20px"
+        height="20px"
+      />
+      <Badge>{getCartItemsQuantity(cartItemsById)}</Badge>
+    </StyledCartButton>
   )
 }
 
