9f1cbf7b30807c7e9330ee7d49293fe521e8cc8e
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-05-28 07:53:08 -0400

    feat(services): add get product by id endpoint

--- src/services/fakestoreapi/index.ts
+++ src/services/fakestoreapi/index.ts
@@ -1,25 +1,28 @@
+// API documentation: https://fakestoreapi.com
+
 import { httpClient } from '#lib/httpClient';
 
 export const apiOrigin = 'https://fakestoreapi.com';
-export const productsEndpoint = `${apiOrigin}/products`;
+export const productsPath = `${apiOrigin}/products`;
 
 type Product = {
-  id: number, // cambiarlo a string
+  id: number,
   title: string,
   price: number,
   description: string,
   category: string,
   image: string, // URL
-  // rating: {
-  //   rate: number,
-  //   count: number,
-  // }
+  rating: {
+    rate: number,
+    count: number,
+  }
 }
 
 type FormatedProduct = Product & {
   quantity: number,
 }
 
+// Type Guard function for expected schema from API
 function isProduct (obj: unknown): obj is Product {
   return (
     typeof obj === 'object' &&
@@ -29,12 +32,14 @@ function isProduct (obj: unknown): obj is Product {
     'id' in obj && typeof obj.id === 'number' &&
     'image' in obj && typeof obj.image === 'string' &&
     'price' in obj && typeof obj.price === 'number' &&
-    'title' in obj && typeof obj.title === 'string'
+    'title' in obj && typeof obj.title === 'string' &&
+    'rating' in obj && typeof obj.rating === 'object'
   );
 } 
 
+// export async function getProducts ({ limit, offset }): Promise<FormatedProduct[]> {
 export async function getProducts (): Promise<FormatedProduct[]> {
-  const products = await httpClient(productsEndpoint);
+  const products = await httpClient(productsPath);
   if (!Array.isArray(products)) throw new Error();
   const formatedProduct = products.map((product) => {
     if (!isProduct(product)) throw new Error();
@@ -45,11 +50,25 @@ export async function getProducts (): Promise<FormatedProduct[]> {
       description: product.description,
       category: product.category,
       image: product.image,
+      rating: { ...product.rating },
       quantity: 20,
     }
   });
-
   return formatedProduct;
 }
 
-// export async frunction getProducById (id: string) {}
+export async function getProductById (id: string): Promise<FormatedProduct> {
+  const product = await httpClient(`${productsPath}/${id}`);
+  if (!isProduct(product)) throw new Error();
+  const formatedProduc = {
+    id: product.id,
+    title: product.title,
+    price: product.price,
+    description: product.description,
+    category: product.category,
+    image: product.image,
+    rating: { ...product.rating },
+    quantity: 20,
+  }
+  return formatedProduc;
+}
