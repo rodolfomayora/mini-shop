cd0c48aaa865c43c9d61d48ade6878f5e8ce083a
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-06-02 18:44:51 -0400

    refactor(components): replace styled-component by tailwind on theme toggle

--- src/components/ToggleThemeButton/index.tsx
+++ src/components/ToggleThemeButton/index.tsx
@@ -1,13 +1,27 @@
-import React, { FC } from 'react';
-
 import { useThemeContext } from '../../context';
-import { ToggleThemeButtonStyled } from './styles';
 
-const ToggleThemeButton: FC = () => {
+/* dark styles backup
+  ${({ theme }) => theme.dark && css`
+    box-shadow: ${colors.dark.shadow};
+    border-color: ${colors.dark.blue3};
+    background-color: ${colors.dark.blue2};
+    background-image: url('/images/svg/moon.svg');
+    background-size: 14px;
+  `}
+*/
+
+export function ToggleThemeButton () {
 
   const { toggleTheme } = useThemeContext();
 
-  return <ToggleThemeButtonStyled onClick={toggleTheme} />;
+  return <input className="
+      flex justify-center w-8 h-8
+      rounded-full border-2 border-[var(--color-yellow)]
+      cursor-pointer
+      bg-[url('/images/svg/sun.svg')] bg-center bg-[length:20px]
+      bg-no-repeat bg-[var(--color-white)]
+    "
+    type="button"
+    onClick={toggleTheme}
+  />;
 }
-
-export default ToggleThemeButton;
