ed124e192594c41e486614fac764c7a7b7e30a68
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-06-02 15:23:08 -0400

    feat(components): add button component and its corresponding tests

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import type { ComponentProps, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type Props = ComponentProps<'button'> & {
+  children: ReactNode,
+}
+
+export function Button ({ children, className = '', ...props }: Props) {
+  const classes = twMerge(`
+    grid place-content-center h-[40px] px-[20px]
+    border-solid border-[2px] border-transparent rounded-[4px]
+    font-[var(--font-montserrat)] text-[16px] text-[var(--color-white)]
+    bg-[var(--color-black-3)] cursor-pointer
+    transition-colors duration-400 ease-linear
+    hover:bg-[var(--color-marine-blue)]
+    disabled:border-[var(--color-gray)]
+    disabled:bg-[var(--color-gray)]
+    disabled:cursord-not-allowed disabled:opacity-50
+    ${className}
+  `);
+  
+  return (
+    <button className={classes} type="button" {...props}>
+      {children}
+    </button>
+  );
+}
