babb02911a47c255f463337746317783861cc2d4
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-06-03 08:32:51 -0400

    refactor(pages): tailwind-css implementation on cart page

--- src/pages/cart/index.page.tsx
+++ src/pages/cart/index.page.tsx
@@ -4,14 +4,6 @@ import { CartItem } from '#components/CartItem';
 import { Layout } from '#components/Layout';
 import { Button } from '#components/Button';
 import { useCart } from '../../context';
-import {
-  MainTitle,
-  CartContent,
-  CartList,
-  CartSummary,
-  NoProductsMessage,
-  TotalPriceLabel,
-} from './styles';
 
 function Cart () {
 
@@ -33,33 +25,37 @@ function Cart () {
   );
 
   const productsToRender = doesCartHasProducts ? allCartItemsId.map(setCartItems) : (
-    <NoProductsMessage>
-      There are no products in the shopping carts
-    </NoProductsMessage>
+    <p className="font-extrabold grid place-content-center w-full h-[40svh]">
+      There are no products added to cart
+    </p>
   );
 
   return (
     <Layout pageTitle="Cart">
-      <MainTitle>CART</MainTitle>
+      <h1 className="mt-0 mb-[40px] text-left text-[var(--color-black-2)]">
+        CART
+      </h1>
 
-      <CartContent>
-        <CartList>
+      <div className="grid gap-y-[40px]">
+        <ul className="grid gap-y-[20px]">
           {productsToRender}
-        </CartList>
+        </ul>
       
-        <CartSummary>
-          <TotalPriceLabel>
-            {`Total Price: $${subTotal}`}
-          </TotalPriceLabel>
+        <p className="
+          py-[10px] px-[10px] rounded-[6px]
+          text-[18px] font-semibold
+          bg-[var(--color-white-2)] shadow-[0_1px_5px_3px_#b1b9d8]
+        ">
+          {`Total Price: $${subTotal}`}
+        </p>
 
-          <Button className="w-full"
-            onClick={redirectToCheckout}
-            disabled={isButtonEnable}
-          >
-            Go to Check out
-          </Button>
-        </CartSummary>
-      </CartContent>
+        <Button className="w-full"
+          onClick={redirectToCheckout}
+          disabled={isButtonEnable}
+        >
+          Go to Check out
+        </Button>
+      </div>
     </Layout>
   );
 }
