48d05d8c17ef5bd1f4a99f5f23eeeb42bf8ad2ab
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-06-03 08:29:41 -0400

    refactor(components): tailwind-css implementation on car-button

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,24 +1,49 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useCart } from '../../context';
-import { StyledCartButton, Badge, CartIcon } from './styles';
+
+/* dark theme backup: badge
+  ${({ theme }) => theme.dark && css`
+    background-color: ${colors.dark.black};
+  `}
+*/
 
 export function CartButton () {
   const cartContext = useCart();
   const { cartState } = cartContext;
   const { totalCartItemsQuantity } = cartState;
+  const is100Producs = totalCartItemsQuantity > 99;
+  const quantityToRender = is100Producs
+    ? '+99'
+    : totalCartItemsQuantity
+    ;
 
   const router = useRouter();
   const redirect = router.push;
   const onHandleClick = () => redirect('/cart');
 
   return (
-    <StyledCartButton onClick={onHandleClick}>
-      <CartIcon src="/images/svg/shoppingCart.svg"
-        alt="Cart"
-        width="20px"
-        height="20px"
+    <button type="button"
+      className="relative bg-transparent cursor-pointer"
+      onClick={onHandleClick}
+      aria-label="Open Cart Page"  
+    >
+      <Image className="size-[18px]"
+        src="/images/svg/shoppingCart.svg"
+        alt="Cart icon"
+        width="18"
+        height="18"
       />
-      <Badge>{totalCartItemsQuantity}</Badge>
-    </StyledCartButton>
+      <div className="
+        absolute top-[100%] start-0 z-1
+        translate-x-[-45%] translate-y-[-54%]
+        grid place-items-center
+        text-[var(--color-white)] text-[12px] 
+        rounded-full bg-[var(--color-black-3)]
+        min-w-[20px] aspect-square pr-[1px] pb-[1px]
+      ">
+        {quantityToRender}
+      </div>
+    </button>
   )
 }
